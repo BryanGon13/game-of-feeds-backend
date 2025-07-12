bc9dabaffe92c203957f9356733148b424f4a50b
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-07-12 05:48:17 +0100

    feat(posts): add image validation for size, width, and height in PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,22 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError("Image size is larger than 2MB!" )
+        if value.size.width > 4096:
+            raise serializers.ValidationError("Image width is larger than 4096px!")
+        if value.size.height > 4096:
+            raise serializers.ValidationError("Image height is larger than 4096px!")
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Post
