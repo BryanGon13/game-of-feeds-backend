f4c65e680d8fa15c332dc7df919593613bb28681
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-07-19 06:39:35 +0100

    feat: add followers resource with model, serializer, and generic views; refactor post and profile views

---
+++ followers/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Follower
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+    
+    class Meta:
+        model = Follower
+        fields = '__all__'
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'You already follow this user.'})
+        
