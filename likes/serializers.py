bf9256cad6414750551f0d74c29424b9b0821a0c
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-07-19 06:01:23 +0100

    feat(likes): add likes app with model, serializer, views, and routing

---
+++ likes/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Like
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+    class Meta:
+        model = Like
+        fields = '__all__'
---
+++ likes/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from game_of_feeds_backend.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
+
+# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
