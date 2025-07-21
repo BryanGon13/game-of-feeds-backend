6759adb13d7f5f69b2431e41bbd3c9182822c200
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-07-21 04:41:45 +0100

    chore: move env.py to project root and configure PostgreSQL for deployment

--- game_of_feeds_backend/env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary://<558315847531383>:<uh_W9GYV5C-bF8ZOGwvqftr-pJc>@dctqmaht5'
-os.environ['DEV'] = '1'
--- game_of_feeds_backend/settings.py
+++ game_of_feeds_backend/settings.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -128,13 +129,17 @@ WSGI_APPLICATION = 'game_of_feeds_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
