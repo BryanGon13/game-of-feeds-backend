6b62830cbb1c136ae517821baccc9404e53b727e
Bryan Gonzalez <bryangonzalez@MacBook-Pro.local>
2026-03-04 10:39:22 +0000

    Fix PostSerializer image field to allow multipart uploads

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,42 +1,39 @@
 from rest_framework import serializers
 from .models import Post
-from likes.models import Like 
+from likes.models import Like
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.profile_image.url')
-    image = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False, allow_null=True)
     likes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    like_id = serializers.SerializerMethodField() 
+    like_id = serializers.SerializerMethodField()
     comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError("Image size is larger than 2MB!")
-        if value.image.width > 4096:
-            raise serializers.ValidationError("Image width is larger than 4096px!")
-        if value.image.height > 4096:
-            raise serializers.ValidationError("Image height is larger than 4096px!")
+        if hasattr(value, "image"):
+            if value.image.width > 4096:
+                raise serializers.ValidationError("Image width is larger than 4096px!")
+            if value.image.height > 4096:
+                raise serializers.ValidationError("Image height is larger than 4096px!")
         return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_like_id(self, obj):  
+    def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
-    
-    def get_image(self, obj):
-        if obj.image and hasattr(obj.image, 'url'):
-            return obj.image.url
-        return ""
 
     class Meta:
         model = Post
