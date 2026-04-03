17ee16dd7724fbacc4c10c14b3bff00268b6fbb4
Bryan Gonzalez <bryangonzalez@MacBook-Pro.local>
2026-04-03 02:42:59 +0100

    Fix profile image not displaying by correcting serializer source to use profile_image field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,7 +6,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-    profile_image = serializers.ReadOnlyField(source='image.url')
+    profile_image = serializers.ReadOnlyField(source='profile_image.url')
     posts_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
