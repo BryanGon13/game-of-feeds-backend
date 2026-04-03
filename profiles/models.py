1b1ee6f4a049e4c45834f731c9e73bfa488a9ba7
Bryan Gonzalez <bryangonzalez@MacBook-Pro.local>
2026-04-03 03:35:39 +0100

    Fix profile image storage by replacing ImageField with CloudinaryField

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -8,8 +9,8 @@ class Profile(models.Model):
     username = models.CharField(max_length=150, unique=True)
     display_name = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
-    profile_image = models.ImageField(
-        upload_to='game_of_feeds/profile_images/', 
+    profile_image = CloudinaryField(
+        'image',
         default='game_of_feeds/default_profile_idzhze'
     )
     house = models.CharField(max_length=100, blank=True) # (Stark, Lannister etc...)
