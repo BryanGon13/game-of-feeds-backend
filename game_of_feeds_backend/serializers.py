d75a6dc54ec3ecb47296e40fece6def2dded0c77
Bryan Gonzalez <bryangonzalez@MacBook-Pro.local>
2026-04-03 04:47:05 +0100

    Fix current user serializer to return secure profile image URL for navbar avatar

--- game_of_feeds_backend/serializers.py
+++ game_of_feeds_backend/serializers.py
@@ -4,7 +4,15 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.profile_image.url')
+    profile_image = serializers.SerializerMethodField()
+
+    def get_profile_image(self, obj):
+        default_url = 'https://res.cloudinary.com/dctqmaht5/image/upload/v1752109202/default_profile_idzhze.jpg'
+        stored = str(obj.profile.profile_image) if obj.profile.profile_image else ''
+        if not stored or stored == 'game_of_feeds/default_profile_idzhze':
+            return default_url
+        url = obj.profile.profile_image.url
+        return url.replace('http://', 'https://', 1)
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
